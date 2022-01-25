9cf6705776f4aa9e86ea723e3dc0ccdb42a3fc8a
jobinpv <96946338+jobinpv@users.noreply.github.com>
2022-01-25 11:37:36 +0530

    Update abc.js

--- js/abc.js
+++ js/abc.js
@@ -74,37 +74,3 @@
       }
     }
 
-var val=document.getElementById('pwd').value;
-    var status=document.getElementById('length');
-    var counter=0;
-
-    if(val!=''){
-        if(val.length<=3)
-        counter=1;
-        if(val.length>3 && val.length<=6)
-        counter=2;
-        if(val.length>=8)
-        counter=3;
-
-        if(counter==1){
-            status.innerHTML="Too WEAK";
-            status.style.backgroundColor="red";
-            status.style.color="white"
-
-        }
-        if(counter==2){
-            status.innerHTML="WEAK";
-            status.style.backgroundColor="orange";
-            status.style.color="white"
-
-        }
-        if(counter==3){
-            status.innerHTML="STRONG";
-            status.style.backgroundColor="green";
-            status.style.color="white"
-
-        }
-
-        
-    }
-}

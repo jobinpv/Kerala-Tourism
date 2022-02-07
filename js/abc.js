866efcca620b515252bd003c0fafbb29ba6cd404
jobinpv <96946338+jobinpv@users.noreply.github.com>
2022-02-08 02:21:01 +0530

    js1

--- js/abc.js
+++ js/abc.js
@@ -13,136 +13,160 @@
         y.style.left= "450px";
         z.style.left= "0";
     }
+    
+    //login validate
 
-    //Phone number
+    var mail = document.getElementById("loguser");
+    var pswd = document.getElementById("logpass");
+    function lvalidate()
+    {
+        var res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var psr = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        
+    
+        if(res.test(mail.value)!="1")
+        {
+            window.alert("Email Is Invalid");
+            return false;
+        }
+        else if(psr.test(pswd.value)!="1")
+        {
+            window.alert("Password Is Invalid, try again");
+            window.alert("Password should be of minimum 8 characters, at least one uppercase, and one lower case, must contain at least one number");
+    
+            return false;
+        }
+        else
+        {
+            window.alert("validation Success");
+            return true;
+        }
+        
+    };
 
-let error1=document.getElementById("error1");
-let phonenumber=document.getElementById("pho")
+//signup validation
 
+var smail = document.getElementById("semail");
+var spswd = document.getElementById("spwd");
+var spswd2 = document.getElementById("srpwd");
+var phone = document.getElementById("spho");
 
-function phvalid()
+function svalidate()
 {
-var phoneno=/^\d{10}$/;
-var phoneno1=/^\(?([0-9]{3})\)?[-.]?([0-9]{3})[-.]?([0-9]{4})$/;
-if(phoneno.test(phonenumber.value))
-{
-    error1.innerHTML="valid";
-error1.style.color="green";
-    return true;
-}
-else if(phoneno1.test(phonenumber.value))
-{error1.innerHTML="valid";
-error1.style.color="green";
-    return true;
-}
-else{
-    alert("Enter only 10 numbers in the phone number field and follow this 3 formats(XXX-XXX-XXXX,XXX.XXX.XXXX,XXX XXX XXXX)");
-    error1.innerHTML="invalid";
-      error1.style.color="red";
-    return false;
-}
-}
+    var res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var psr = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    
+
+    if(res.test(smail.value)!="1")
+    {
+        
+        window.alert("Email Is Invalid");
+        return false;
+    }
+    else if(phoneno.test(phone.value)!="1")
+    {
+        window.alert("Please enter a valid phone number :");
+        return false;
+    }
+    else if(psr.test(spswd.value)!="1")
+    {
+        window.alert("Password Is Invalid");
+        window.alert("Password should be of minimum 8 characters, at least one uppercase, and one lower case, must contain at least one number and special character");
+        window.alert("try again");
+        return false;
+    }
+    else  if(spswd.value!=spswd2.value)
+    {
+        window.alert("Passwords Doesnt Match, Please Re-enter :");
+        return false;
+    }
+    else
+    {
+        window.alert("validation Success");
+        return true;
+    }
+    
+};
 
 
-//Password
-const pwd=document.getElementById('pwd');
-const cpwd=document.getElementById('cpwd');
-const pho=document.getElementById('pho');
+$(document).ready(function () {
 
-function validation() {
-  const pwdValue = pwd.value.trim(); 
-  const cpwdValue = cpwd.value.trim();
-  const phoValue = pho.value.trim();
+    $('#spwd').on('keyup', function () {
 
-  if(phoValue===''){
-    //show error
-    //add error class
-    setErrorFor(pho, 'Mobile number cannot be blank');
-    validation = false;}
-    else if(!ispho(phoValue)){
-        setErrorFor(pho, 'Mobile number is not valid');  
-        validation = false;}
-        else{
-    //add success class
-    setSuccessFor(pho);
-}
+        let textElement = $(this).val()
+        let strength = 0
 
-  if(pwdValue===''){
-    //show error
-    //add error class
-    setErrorFor(pwd, 'Password cannot be blank');
-    validation = false;}
-    else if(!isPwd(pwdValue)){
-        setErrorFor(pwd, 'Password is not valid');  
-        validation = false;}
-        else{
-    //add success class
-    setSuccessFor(pwd);
-}
-if(cpwdValue===''){
-    //show error
-    //add error class
-    setErrorFor(cpwd, 'This field cannot be blank');
-    validation = false;}
-    else if(pwdValue!=cpwdValue){
-        setErrorFor(cpwd, 'Password does not match');  
-        validation = false;}
-        else{
-    //add success class
-    setSuccessFor(cpwd);
-}
+        $('#typepass').find('h4').html(`Your Password: ${textElement}`)
 
-return validation; 
+        if (textElement.length > 0) {
+            let sizeElements = textElement.length
 
-}
+            if (sizeElements > 10) {
 
-function ispho(pho){
-  return /^\+?([6-9]{1})([\d]{2})[\-.\s]?([\d]{3})[\-.\s]?([\d]{4})$/.test(pho)
-}
-function isPwd(pwd){  
-  return /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{8,})$/.test(pwd)
-}
+                strength += 30
 
-// For password strength
+            } else {
+                let calcMath = (sizeElements * 2)
 
-pwd.addEventListener('keyup', function(){
-  strength();
-})
-function strength(){
-  var val=document.getElementById('pwd').value;
-  var status=document.getElementById('length');
-  var counter=0;
+                strength += calcMath
 
-  if(val!=''){
-      if(val.length<=3)
-      counter=1;
-      if(val.length>3 && val.length<=6)
-      counter=2;
-      if(val.length>=8)
-      counter=3;
+            }
 
-      if(counter==1){
-          status.innerHTML="Too WEAK";
-          status.style.backgroundColor="red";
-          status.style.color="white"
+        }
 
-      }
-      if(counter==2){
-          status.innerHTML="WEAK";
-          status.style.backgroundColor="orange";
-          status.style.color="white"
+        let lowerCase = new RegExp(/[a-z]/)
+        if (lowerCase.test(textElement)) {
+            strength += 16
+        }
 
-      }
-      if(counter==3){
-          status.innerHTML="STRONG";
-          status.style.backgroundColor="green";
-          status.style.color="white"
+        let upperCase = new RegExp(/[A-Z]/)
+        if (upperCase.test(textElement)) {
+            strength += 18
+        }
 
-      }
+        let regularNumber = new RegExp(/[0-9]/i)
+        if (regularNumber.test(textElement)) {
+            strength += 16
+        }
 
-      
-  }
-}
+        let specialChars = new RegExp(/[^a-z0-9]/i)
+        if (specialChars.test(textElement)) {
+            strength += 20
+        }
+        
+        //Function to produce result
+        let renderResult = (strengthData, color) => {
+            return $('#strengthResult').html(
+                `
+                          
+            <div class="progress" style="height: 40px;">
+                <div class="progress-bar bg-${color}" role="progressbar" style="width: ${strengthData}%" aria-valuenow="${strengthData}" aria-valuemin="0" aria-valuemax="100"><strong style="font-size: 15px">${strength}%</strong></div>
+            </div>`
+            )
+        }
+
+        if (strength < 21) {
+            renderResult(strength, 'danger')//red very weak password
+        } else
+            if (strength > 20 && strength < 41) {
+                renderResult(strength, 'warning')//orange weak password
+            } else
+                if (strength > 40 && strength < 61) {
+                    renderResult(strength, 'secondary')//medium password
+                } else
+                    if (strength > 60 && strength < 81) {
+                        renderResult(strength, 'info')// strong password
+                    } else {
+                        renderResult(strength, 'success')//very strong password
+                    }
+
+        
+
+    });
+
+});
+
 
 
 
